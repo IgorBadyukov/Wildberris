199c249a4f15e2216a9f42ca05d496b84b7282c9
IgorBadyukov <badyukov1806302003@gmail.com>
2021-11-21 23:48:36 +0300

    Добавил рендер товаров + кнопка view all

--- js/getgoods.js
+++ js/getgoods.js
@@ -1,20 +1,66 @@
 function getGoods() {
-    const links=document.querySelectorAll('.navigation-link');
+    const links = document.querySelectorAll('.navigation-link');
+    const viewAll=document.querySelector('.more');
 
-    const getData=()=>{
-        fetch('https://wildberris-d90b0-default-rtdb.europe-west1.firebasedatabase.app/db.json')
-        .then(response=> response.json())
-        .then(data=>{
-            localStorage.setItem('goods', JSON.stringify(data));
+    const renderGoods = (goods) => {
+        const goodsContainer = document.querySelector('.long-goods-list');
+        goodsContainer.innerHTML='';
+
+        goods.forEach(good => {
+            const goodBlock = document.createElement('div');
+            goodBlock.classList.add('col-lg-3');
+            goodBlock.classList.add('col-sm-6');
+
+            goodBlock.innerHTML = `
+            <div class="goods-card">
+                <span class="label ${good.label ? 'null' : 'd-none'}">${good.label}</span>
+                <img src="db/${good.img}" alt="${good.label}" class="goods-image">
+                <h3 class="goods-title">${good.name}</h3>
+                <p class="goods-description">${good.description}</p>
+                <button class="button goods-card-btn add-to-cart" data-id="${good.id}">
+                     <span class="button-price">$${good.price}</span>
+                </button>
+            </div>`;
+            goodsContainer.append(goodBlock);
         });
     };
 
-    links.forEach(link=>{
-        link.addEventListener('click',event=>{
+    const getData = (value, category) => {
+        fetch('https://wildberris-d90b0-default-rtdb.europe-west1.firebasedatabase.app/db.json')
+            .then(response => response.json())
+            .then(data => {
+                const array = category ? data.filter((item) => item[category] === value) : data;
+                localStorage.setItem('goods', JSON.stringify(array));
+                console.log(array);
+
+                if (window.location.pathname !== '/goods.html') {
+                    window.location.href = '/goods.html';
+                } else {
+                    renderGoods(array);
+                }
+            });
+    };
+
+    links.forEach(link => {
+        link.addEventListener('click', (event) => {
             event.preventDefault();
-            getData();
+            const linkValue = link.textContent;
+            const category = link.dataset.field;
+            getData(linkValue, category);
         });
     });
+
+    if(window.location.pathname === '/index.html'){
+        viewAll.addEventListener('click', event=>{
+            event.preventDefault();
+            getData('All', false);
+        });
+    }
+   
+
+    if (JSON.parse(localStorage.getItem('goods')) && window.location.pathname === '/goods.html') {
+        renderGoods(JSON.parse(localStorage.getItem('goods')));
+    }
 }
 
 getGoods();
